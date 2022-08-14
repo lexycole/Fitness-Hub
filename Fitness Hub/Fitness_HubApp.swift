7b31c0763f2083dc75ba1e9a750fadc476fe6cdf
Olawale Akinro <akinbolawa90@gmail.com>
2022-08-14 10:27:36 -0700

    Initial Commit

---
+++ Fitness Hub/Fitness_HubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_HubApp.swift
+//  Fitness Hub
+//
+//  Created by swift-rn-flutter on 8/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_HubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
